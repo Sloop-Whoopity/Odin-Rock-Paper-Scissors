a4038a009799ddc989f1f35025eeae32cc3b1080
elijah_ubuntu <elijah.pitt@proton.me>
2024-03-09 22:25:28 -0700

    Fix Small Bugs Regarding Ties and Bad Choices
    Fixed small bugs that would allow user to input an invalid choice
    or would result in a true tie value persisting after the
    round that results in a tie.

--- Script.js
+++ Script.js
@@ -15,7 +15,8 @@ function getComputerChoice() {
     if (decision >= 1 && decision < 4) {computerSelection = "Rock";
     } else if (decision >= 4 && decision < 7) {computerSelection = "Paper";
     } else  if (decision >= 7 && decision <= 9) {computerSelection = "Scissors";
-    } else {getComputerChoice(); //Will run through console.log(computerChoice) twice because it goes through the function once, then completes the original function again.  
+    } else {getComputerChoice(); //Will run through console.log(computerChoice) twice because 
+    // it goes through the function once, then completes the original function again.  
     }
 
     console.log(computerSelection)
@@ -26,7 +27,9 @@ function playRound() {
     let beginningLetter = playerWord.at(0);
     let slicedWord = playerWord.slice(1);
     let playerSelection = beginningLetter.toUpperCase() + slicedWord.toLowerCase();
+
     console.log("Conjoined Word", playerSelection)
+
     if (playerSelection === "Rock" && computerSelection === "Scissors") {playerWins = true;
     } else if (playerSelection === "Paper" && computerSelection === "Rock") {playerWins = true;
     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {playerWins = true;
@@ -36,8 +39,11 @@ function playRound() {
     console.log("playerWins", playerWins)
 
     if (playerWins === true) {alert(`You Win! ${playerSelection} " beats " ${computerSelection}`);
-    } else if (playerWins === false && playerSelection === computerSelection) {tie = true, alert(`Tie! ${playerSelection} matches ${computerSelection}`);
-    } else {alert(`You Lose! ${computerSelection} " beats " ${playerSelection}`);
+    } else if (playerWins === false && playerSelection === computerSelection) 
+        {tie = true, alert(`Tie! ${playerSelection} matches ${computerSelection}`);
+    } else {
+        alert(`You Lose! ${computerSelection} " beats " ${playerSelection}`);
+        tie = false;
     }
 
     console.log("tie", tie)
@@ -48,17 +54,31 @@ function playGame() {
     alert("Are you ready to play ROCK, PAPER, SCISSORS?!?!");
     playerWord = prompt("Do you pick rock, paper, or scissors?", "");
     getComputerChoice()
-    playRound()
+
+    let badChoice = false;
+    playerWins = false;
+    if (playerWord.toLowerCase() === "rock"
+        || playerWord.toLowerCase() === "paper" 
+        || playerWord.toLowerCase() === "scissors"
+        ) {playRound();
+        } else {
+            alert("I only gave you three choices. And yet? You chose poorly!");
+            badChoice = true;
+        }
+
     if (playerWins === true) {playerScore++;
     } else if (playerWins === false && tie === true) {alert("There was a tie! No one gets any points!");
-    } else {computerScore++;
+    } else if (badChoice !== true) {computerScore++;
     }
     
     console.log("Player Score: ", playerScore)
     console.log("Computer Score: ", computerScore)
 
     let anotherGame = prompt("Would you like to play again?", "")
-    if (anotherGame.toLowerCase() === "no" || anotherGame == null || anotherGame.toLowerCase() === "No Thank You") {alert("Game Over");
+
+    if (anotherGame.toLowerCase() === "no" || 
+    anotherGame == null || 
+    anotherGame.toLowerCase() === "No Thank You") {alert("Game Over");
     } else {playGame();
     }
 }
